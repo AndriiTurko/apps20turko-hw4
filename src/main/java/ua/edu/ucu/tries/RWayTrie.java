7f336d10b3ed47e32ed80ca38143b6e4beee9cbc
AndriiTurko <turko@ucu.edu.ua>
2020-12-03 21:16:42 +0200

    final commit

--- src/main/java/ua/edu/ucu/tries/RWayTrie.java
+++ src/main/java/ua/edu/ucu/tries/RWayTrie.java
@@ -36,7 +36,7 @@ public class RWayTrie implements Trie {
             size++;
         }
         if (i+1 == s.length()) { node.v = value; return node; }
-        if (node.next[s.charAt(i+1)] == null) node.n++;
+        if (node.next[s.charAt(i+1)] == null) { node.n++; };
         node.next[s.charAt(i+1)] = addHelper(node.next[s.charAt(i+1)], s, i+1, value);
         return node;
     }
@@ -47,13 +47,13 @@ public class RWayTrie implements Trie {
     }
 
     private boolean containsHelper(Node node, String s, int i) {
-        if (i<s.length() && node == null)return false;
+        if (i<s.length() && node == null) { return false; }
         else if (i == s.length()-1) {
             if (node.v!=null) {
                 System.out.println(node.v);
                 return true;
-            } else return false;
-        } else return containsHelper(node.next[s.charAt(i+1)], s, i+1);
+            } else { return false; }
+        } else { return containsHelper(node.next[s.charAt(i+1)], s, i+1); }
     }
 
     @Override
@@ -62,7 +62,7 @@ public class RWayTrie implements Trie {
         if (r && (--trieRoot.next[word.charAt(0)].n) == 0) {
             trieRoot.next[word.charAt(0)] = null;
             return true;
-        } else return false;
+        } else { return false; }
     }
 
     private boolean deleteHelper(Node node, String s, int i) {
@@ -76,7 +76,7 @@ public class RWayTrie implements Trie {
             if (t && (--node.next[s.charAt(i+1)].n) == 0) {
                 node.next[s.charAt(i+1)] = null;
                 return true;
-            } else return false;
+            } else { return false; }
         }
     }
 
@@ -92,13 +92,6 @@ public class RWayTrie implements Trie {
         return q;
     }
 
-    @Override
-    public Iterable<String> wordsWithPrefix(String s, int k) {
-        Queue q = new Queue(new ImmutableLinkedList());
-        collect(get(trieRoot, s, 0), s, q);
-        return q;
-    }
-
     private void collect(Node x, String pre, Queue q) {
         if (x == null) return;
         if (x.v != null) q.enqueue(pre);
@@ -106,10 +99,9 @@ public class RWayTrie implements Trie {
             collect(x.next[c], pre + c, q);
     }
 
-    private Node get(Node x, String key, int d)
-    { // Return node associated with key in the sub trie rooted at x.
-        if (x == null) return null;
-        if (d == key.length()) return x;
+    private Node get(Node x, String key, int d) {
+        if (x == null) { return null; }
+        if (d == key.length()) { return x; }
         char c = key.charAt(d); // Use dth key char to identify sub trie.
         return get(x.next[c], key, d+1);
     }
@@ -118,8 +110,4 @@ public class RWayTrie implements Trie {
     public int size() {
         return size;
     }
-
 }
-
-
-
